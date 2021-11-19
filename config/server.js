7a00e849bd0e9e65c423ae58cf2ddff914e31d6e
Cris Lom <bigc4997@gmail.com>
2021-11-19 09:13:02 -0500

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '693e0d5776cd8cb2291f1791066a0569'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9a3770af-89ae-4f58-95c7-0bb7c4228369'
+};
