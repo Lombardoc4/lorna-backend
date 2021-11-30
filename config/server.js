d12464b9311f572a98ce35e4ac901ea3ac485453
Cris Lom <bigc4997@gmail.com>
2021-11-30 07:32:27 -0500

    fixing dev server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('STRAPI_URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '693e0d5776cd8cb2291f1791066a0569'),
