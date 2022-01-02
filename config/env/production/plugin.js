9812864b6c6c9811661d1584043bc2ed9ec769ea
Cris Lom <bigc4997@gmail.com>
2022-01-02 09:40:04 -0500

    updating to s3 storage

--- config/env/production/plugin.js
+++ config/env/production/plugin.js
@@ -1,13 +1,24 @@
 module.exports = ({ env }) => ({
+  // upload: {
+  //   provider: "aws-s3",
+  //   providerOptions: {
+  //     key: env('DO_SPACE_ACCESS_KEY'),
+  //     secret: env('DO_SPACE_SECRET_KEY'),
+  //     endpoint: env('DO_SPACE_ENDPOINT'),
+  //     space: env('DO_SPACE_BUCKET')
+  //   }
+  // }
   upload: {
-    provider: "do",
+    provider: 'aws-s3',
     providerOptions: {
-      key: env('DO_SPACE_ACCESS_KEY'),
-      secret: env('DO_SPACE_SECRET_KEY'),
-      endpoint: env('DO_SPACE_ENDPOINT'),
-      space: env('DO_SPACE_BUCKET')
-    }
-  }
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET_NAME'),
+      },
+    },
+  },
 });
 
 // Not sure if this or extensions/upload/config/settings is working?
