e8f69d3b5e0b8863fb972e0a8a1f0f884b8caee5
Cris Lom <bigc4997@gmail.com>
2022-01-03 13:51:39 -0500

    thumbnail middlewares

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,11 +1,11 @@
-// module.exports = {
-//     "provider": "aws-s3",
-//     "providerOptions": {
-//         "accessKeyId": "AKIA46PGPQ5KI4YKGI47",
-//         "secretAccessKey": process.env.AWS_ACCESS_SECRET,
-//         "region": "us-east-1",
-//         "params": {
-//             "Bucket": "lornashore"
-//         }
-//     }
-// }
+module.exports = {
+    "provider": "aws-s3",
+    "providerOptions": {
+        "accessKeyId": "AKIA46PGPQ5KI4YKGI47",
+        "secretAccessKey": process.env.AWS_ACCESS_SECRET,
+        "region": "us-east-1",
+        "params": {
+            "Bucket": "lornashore"
+        }
+    }
+}
